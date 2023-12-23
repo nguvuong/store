031d5d9c1851609f05d015f532eb187ca9ddbcd2
Vuong Nguyen <nguvuong@sheridancollege.ca>
2023-12-23 01:58:46 -0500

    finished configuring the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <app-header></app-header>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
 
-  <div class="w-100px flex justify-between">
-    <div>01</div>
-    <div>02</div>
-    <div>03</div>
-</div>
+
   `,
   styles: []
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/components/**/*.{html,js}',
+    './src/app/components/**/*.{html,js,ts}',
 
   ],
   theme: {
